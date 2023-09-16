3697f97df5d1df8339a960dad70068f43c0c49cb
thourihan <thourihan@uri.edu>
2023-09-16 17:56:31 -0400

    added read csv function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+import csv
 
 app = FastAPI()
 
+# Function to read CSV and return list of reviews
+def read_csv(filename: str) -> list:
+    reviews = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            reviews.append(row['Review'])
+    return reviews
+
+reviews_list = read_csv('dummy_data.csv')  # replace 'your_csv_filename.csv' with the actual name of your CSV file
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"reviews": reviews_list}
