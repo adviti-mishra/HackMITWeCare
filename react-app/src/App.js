c851ed96a2a5754bc3f94d66dc928257a296bf98
adviti <adviti@umich.edu>
2023-09-17 02:17:43 -0400

    IT WORKS - dummy data for now / other link

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,10 +4,13 @@ function App() {
   return (
     <div className="App">
       <main>
-        {/* Render the EmbeddedMap component */}
+        {/* Render the EmbeddedMap component
+        src="https://kepler.gl/demo?mapUrl=https://gist.githubusercontent.com/adviti-mishra/e5e432fb8776b59e0055d845a0d82b59/raw/ad2cf4761fc7fffdff50d6b122a500ba3fd458d6/mapBos.json"
+        
+        */}
         <iframe
           title="Embedded HTML"
-          src="https://kepler.gl/demo?mapUrl=https://gist.githubusercontent.com/adviti-mishra/e5e432fb8776b59e0055d845a0d82b59/raw/ad2cf4761fc7fffdff50d6b122a500ba3fd458d6/mapBos.json"
+          src="https://kepler.gl/demo?mapUrl=https://gist.githubusercontent.com/adviti-mishra/45913b66bc14a0f2363cd98f77e62fb0/raw/19216c90bdff01fa61be830ea8197864318c4b36/mapFinal.json"
           width="100%"
           height="400px"
           style={{ overflow: "hidden" }}
