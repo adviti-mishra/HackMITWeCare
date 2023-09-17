152f7357f3d0c65c80a5cc319e55564ee7ce3ab0
adviti <adviti@umich.edu>
2023-09-16 21:56:39 -0400

    React cleaning up redux mess

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { KeplerGl } from "kepler.gl";
 
-const mapBoxAPIKey = process.env.MAPBOX_TOKEN;
+const mapBoxAPIKey = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function App() {
   // Kepler.gl configuration to be centered in Masacheusettes
   const config = {
     mapState: {
       latitude: 42.4072,
-      longitude: 71.3824,
+      longitude: -71.3824,
       zoom: 8,
     },
   };
@@ -17,7 +18,15 @@ function App() {
       <header>
         <h1> We Care </h1>
       </header>
-      <main>Kepler Map</main>
+      <main>
+        <div>
+          {/* <KeplerGl
+            id="app-map"
+            mapboxApiAccessToken={mapBoxAPIKey}
+            initialConfig={config}
+          ></KeplerGl> */}
+        </div>
+      </main>
     </div>
   );
 }
