f30ddbc53e552458a2f04634f5f62ea06ab50a0c
thourihan <thourihan@uri.edu>
2023-09-16 21:02:32 -0400

    yelp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from constants import IBM_API_KEY
 from constants import YELP_API_KEY
 import csv
+import requests
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -40,10 +41,30 @@ def analyze_sentiment(reviews: list) -> list:
 
 
 # Function to get all yelp reviews from a specific business id and returns a list
-def get_yelp_reviews(business_id: str) -> list:
-    return
+def get_yelp_reviews(business_id: str):
+    # API endpoint
+    headers = {
+        'Authorization': f"Bearer {YELP_API_KEY}",
+    }
+
+    url = f"https://api.yelp.com/v3/businesses/{business_id}/reviews?limit=20"
+
+    # Send the GET request
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        # Extract the text of the reviews and return
+        for row in data["reviews"]:
+            print(row["text"])
+        return data['reviews']
+    else:
+        # Print error and return empty list if unsuccessful
+        print(f"Error {response.status_code}: {response.text}")
+        return []
 
 
 @app.get("/")
 async def root():
-    return "Hello, world!";
+    return get_yelp_reviews("massachusetts-general-hospital-boston-4")
